0c1b9e9247974ed9879df0af30f5bb5e2912b2fb
itallo2L <itallocerqueiracosta@gmail.com>
2024-12-09 19:19:31 -0300

    Adicionei as classes de dados #time 0h 05m

---
+++ ControleDeGastos.Dominio/Entidades/EntradaFinanceira.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeGastos.Dominio.Entidades
+{
+    public class EntradaFinanceira
+    {
+        public int Id { get; set; }
+        public double ValorRecebido { get; set; }
+        public double GastosEssenciais { get; set; }
+        public double GastosOpcionais { get; set; }
+        public double Investimentos { get; set; }
+    }
+}
---
+++ ControleDeGastos.Dominio/Entidades/SaidaFinanceira.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeGastos.Dominio.Entidades
+{
+    public class SaidaFinanceira
+    {
+        public int Id { get; set; }
+        public double ValorGasto { get; set; }
+        public string Categoria { get; set; }
+        public string Descricao { get; set; }
+    }
+}
